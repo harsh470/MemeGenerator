a18ace5ca80ef3a5a97b52fccde42d66969fd10d
Harsh Pandere <harshpandere47@gmail.com>
2023-11-03 14:33:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Components/Header";
+import Meme from "./Components/Meme";
+
+function App() {
+  return (
+    <>
+   <Header/>
+   <Meme/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <header className="header">
+     <img src="../images/nav-logo.png" alt="navlogo" className="header--image"/>
+    <h2 className="header--title">Meme Generator</h2>
+    <h4 className="header--project">By Harsh Pandere</h4>
+    </header>
+  )
+  
+}
+export default Header
